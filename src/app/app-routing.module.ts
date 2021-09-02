fdbae165bce67f367208f89964cc6ec65fde4338
Jorge Carrizo <jorgitocarrizo@gmail.com>
2021-09-02 20:13:53 -0300

    Primer commit. Modificaciones para deploy heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './page/error/error.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'error',component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { ErrorComponent } from './page/error/error.component';
+import { MenuPrincipalComponent } from './page/menu-principal/menu-principal.component';
+import { FooterComponent } from './page/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    MenuPrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
